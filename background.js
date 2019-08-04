0f42cbdadfd7c974d7ec33033bfbc0c3b120017d
Jamie Allen <Jamieallen59@gmail.com>
2019-08-04 20:50:06 +0100

    redirect to Amazon Smile

--- background.js
+++ background.js
@@ -1,19 +1,53 @@
+// Background.js runs when chrome starts up.
+
+const redirectToAmazonSmile = info => {
+  console.log('Called', info)
+  console.log('URL info: ', info.initiator)
+
+  return { redirectUrl: 'https://smile.amazon.co.uk' };
+}
+const filters = {
+  urls: [
+    "*://www.amazon.co.uk/*"	
+  ],
+  types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
+}
+
 chrome.runtime.onInstalled.addListener(() => {
-
   // storage: Same as local storage
-  chrome.storage.sync.set({color: '#3aa757'}, () => {
-    console.log('SET COLOR')
-  });
+  // chrome.storage.sync.set({color: '#3aa757'}, () => {
+  //   console.log('SET COLOR')
+  // });
 
-  // declarativeContent: Decides when to activate the plugin
+  // https://stackoverflow.com/questions/12065029/redirecting-url-in-a-chrome-extension
+  chrome.webRequest.onBeforeRequest.addListener(
+    redirectToAmazonSmile,
+    filters,
+    ["blocking"]
+  )
+
+  // declarativeCon tent: Decides when to activate the plugin
+  // Note: You should always register or unregister rules in bulk 
+  // rather than individually because each of these operations 
+  // recreates internal data structures. This re-creation is 
+  // computationally expensive but facilitates a faster matching algorithm.
   chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
-    chrome.declarativeContent.onPageChanged.addRules([{
+
+    const WHEN_TO_ACTIVATE_PLUGIN_RULE = {
       conditions: [
+        // https://developer.chrome.com/extensions/declarativeContent#property-PageStateMatcher
         new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: 'developer.chrome.com'},
+          pageUrl: {
+            urlContains: '.amazon.'
+          },
         })
       ],
+      // Triggers page_action declared in manifest.json
       actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
+    }
+
+    chrome.declarativeContent.onPageChanged.addRules([
+      WHEN_TO_ACTIVATE_PLUGIN_RULE
+    ]);
   });
 });
--- popup.js
+++ popup.js
@@ -1,27 +1,24 @@
-let changeColor = document.getElementById('changeColor');
 
-// storage: Same as local storage
-chrome.storage.sync.get('color', data => {
-  const { color } = data
-
-  console.log('GET COLOR')
-
-  changeColor.style.backgroundColor = color;
-  changeColor.setAttribute('value', color);
-});
-
+// .storage: Same as local storage
+// chrome.storage.sync.get('color', data => {
+  //   const { color } = data
+  
+  //   changeColor.style.backgroundColor = color;
+  //   changeColor.setAttribute('value', color);
+  // });
+  
+const changeColor = document.getElementById('tester');
 changeColor.onclick = element => {
-  const { value: color } = element.target
-  console.log('HERE', document.body.style.backgroundColor)
+  // const { value: color } = element.target
 
-  chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
     chrome.tabs.executeScript(
       tabs[0].id,
       { code: "alert('it works!')" }
     )
 
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: 'document.body.style.backgroundColor = "' + color + '";'});
+    // chrome.tabs.executeScript(
+    //     tabs[0].id,
+    //     {code: 'document.body.style.backgroundColor = "' + color + '";'});
   });
 };
